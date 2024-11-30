b723eb52433241b9275780949755dd70e5d3d270
PranavAvasthi <pranavavasthi44@gmail.com>
2024-11-30 18:31:39 +0530

    CORS Middleware

--- app/main.py
+++ app/main.py
@@ -1,13 +1,21 @@
 from fastapi import FastAPI # type: ignore
 from app.routers import git
 from dotenv import load_dotenv # type: ignore
-import os
+from fastapi.middleware.cors import CORSMiddleware # type: ignore
 
 load_dotenv()
 
 app = FastAPI(title="Git Repository Analyzer")
 
 app.include_router(git.router, prefix="/api/v1", tags=["git"])
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True, 
+    allow_methods=["*"], 
+    allow_headers=["*"],  
+)
 
 if __name__ == "__main__":
     import uvicorn # type: ignore
